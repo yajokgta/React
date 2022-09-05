a1deb0d5e75ab24513b823c2c9a2ebcee4dca31d
yajokgta <welovelove93@gmail.com>
2022-09-05 22:22:41 +0700

    Revert "no message"
    This reverts commit 77abf73e5bf3d65a1fde33035bb85d33990ee218.

--- leaning/src/App.js
+++ leaning/src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+var logic = require("./logic.js");
 function App() {
+  var numberbt = 10;
 
+  var shownumber = <label>{numberbt}</label>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <div className="numberlist-item">
+            <h1>{shownumber}</h1>
+          </div>
+        </div>
+          <div>
+          <label>กรอกตัวเลข :</label>&nbsp;
+          <input type="text" className="number" name="number" id="number"></input>&nbsp;
+          <label>บน :</label>&nbsp;
+          <input type="text" className="number" name="bon" id="bon"></input>&nbsp;
+          <label>ล่าง :</label>&nbsp;
+          <input type="text" className="number" name="lang" id="bon"></input>&nbsp;
+          </div>
+      </header>
+    </div>
+    
+  );
 }
 
 export default App;
---
+++ leaning/src/logic.js
@@ -0,0 +1,9 @@
+function GetNumber() {
+    const numberlist = [];
+    var number = document.getElementById('number');
+    console.log(number);
+    if (number.lenght > 2)
+    {
+  
+    }
+  }
